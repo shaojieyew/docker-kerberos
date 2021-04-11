397dac6c9cbbcb14d6c532e316498c8543295106
YSJ <shaojieyew@gmail.com>
2021-04-12 00:51:28 +0800

    -

--- kerberos-client/init-script.sh
+++ kerberos-client/init-script.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
+echo "==================================================================================="
+echo "==== Kerberos Client =============================================================="
+echo "==================================================================================="
+KADMIN_PRINCIPAL_FULL=$KADMIN_PRINCIPAL@$REALM
+echo "REALM: $REALM"
+echo "KADMIN_PRINCIPAL_FULL: $KADMIN_PRINCIPAL_FULL"
+echo "KADMIN_PASSWORD: $KADMIN_PASSWORD"
+echo ""
+function kadminCommand {
+    kadmin -p $KADMIN_PRINCIPAL_FULL -w $KADMIN_PASSWORD -q "$1"
+}
+echo "==================================================================================="
+echo "==== /etc/krb5.conf ==============================================================="
+echo "==================================================================================="
+tee /etc/krb5.conf <<EOF
+[libdefaults]
+        default_realm = EXAMPLE.COM
 
-source `dirname $0`/configureKerberosClient.sh
-
-# Your application
-
-# You can use the `kadminCommand` function to perform kadmin commands. Example:
-# kadminCommand "get_principal yourprincipal@$REALM"
+[realms]
+        EXAMPLE.COM = {
+                kdc_ports = 88,750
+                kadmind_port = 749
+                kdc = kdc-kadmin
+                admin_server = kdc-kadmin
+        }
+EOF
+echo ""
+echo "==================================================================================="
+echo "==== Testing ======================================================================"
+echo "==================================================================================="
+kadminCommand "list_principals $KADMIN_PRINCIPAL_FULL"
+echo "KDC and Kadmin are operational"
+echo ""
